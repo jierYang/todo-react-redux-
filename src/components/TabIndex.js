9790848cdc85831bd2d4eadd53ff56511f441804
Yang Jie <jiyang@thoughtworks.com>
2018-09-13 16:22:03 +0800

    add two child tab in Tab

--- src/components/TabIndex.js
+++ src/components/TabIndex.js
@@ -1,13 +1,15 @@
 import {Tab, Tabs} from "react-bootstrap";
 import React from "react";
+import TabTodo from "./TabTodo";
+import TabStatistic from "./TabStatistic";
 
 const TabIndex = ()=>(
     <Tabs defaultActiveKey={1} id="tabIndex">
         <Tab eventKey={1} title="To dos">
-            Tab 1 content
+            <TabTodo/>
         </Tab>
         <Tab eventKey={2} title="Statistic">
-            Tab 2 content
+            <TabStatistic/>
         </Tab>
     </Tabs>
 );
---
+++ src/components/TabTodo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TabTodo = ()=>(
+    <div>
+        list
+    </div>
+)
+
+export default TabTodo
