873d04114f3c9f7f9c7d158d477b9a07c0b43bb7
Yang Jie <jiyang@thoughtworks.com>
2018-09-24 20:53:25 +0800

    remove frontend id

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import {Provider} from 'react-redux'
 import {combineReducers, createStore} from "redux";
 import {loginContainer, withAuthentication} from "./components/Login/LoginConatiner"
 
-let todoId = 0;
+// let todoId = 0;
 const addTodoReducer = (state = [], action) => {
     switch (action.type) {
         case "ADD_TODO": {
             debugger
-            todoId += 1;
-            action.todo['id'] = todoId;
+            // todoId += 1;
+            // action.todo['id'] = todoId;
             return [...state, action.todo];
         }
 
